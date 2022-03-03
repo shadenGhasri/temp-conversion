cb686e05be2a5ff92923c1dcb4614ca4d32a8dd1
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-03-03 19:16:57 +0330

    add

--- script.js
+++ script.js
@@ -3,22 +3,18 @@ const reset = document.querySelector(".reset")
 const change = document.querySelector(".change")
 const input = document.querySelector(".input")
 const divAlert = document.querySelector(".div-alert")
+const pAlert = document.querySelector(".pAlert")
 const title = document.querySelector(".title")
 
 convert.addEventListener("click",()=>{
     if(input.value === ""){
-        const pAlert = document.createElement("p");
         pAlert.textContent =    `insert correct value`
-        pAlert.style.fontWeight = "bold"
         pAlert.style.color = "red"
-        divAlert.append(pAlert)
+        
     }else if(title.textContent ===  "converter ºC to ºF"){
         let resultValue = (input.value  * 1.8) + 32
-        const pAlert = document.createElement("p");
         pAlert.textContent =    `${input.value}ºC to ${resultValue}ºF`
-        pAlert.style.fontWeight = "bold"
         pAlert.style.color = "black"
-        divAlert.append(pAlert)
 
     }else if(title.textContent ===  "converter ºF to ºC"){
         let resultValue = (input.value -32) * 5 / 9
